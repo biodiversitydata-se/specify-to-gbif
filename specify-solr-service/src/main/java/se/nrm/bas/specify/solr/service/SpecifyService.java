6efa3cd63d47ccab099b66616c0032e096dd5b62
Ida Li <ida.li@nrm.se>
2020-02-28 16:12:35 +0100

    Add queryParams in rest service

--- specify-solr-service/src/main/java/se/nrm/bas/specify/solr/service/SpecifyService.java
+++ specify-solr-service/src/main/java/se/nrm/bas/specify/solr/service/SpecifyService.java
@@ -1,11 +1,15 @@
 package se.nrm.bas.specify.solr.service;
-
+ 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces; 
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
+import se.nrm.bas.specify.solr.service.logic.SolrToGbifLogic; 
+import se.nrm.bas.specify.solr.service.logic.util.Util;
 
 /**
  *
@@ -17,11 +21,19 @@ import lombok.extern.slf4j.Slf4j;
 public class SpecifyService {
    
   private final String success = "Done";
+   
+  @Inject
+  private SolrToGbifLogic logic;
   
   @GET
   @Path("/run") 
   @Produces(MediaType.APPLICATION_JSON)
-  public Response run() {  
+  public Response run(@QueryParam("inst") String institution, @QueryParam("collCode") String collectionCode,
+          @QueryParam("from") String fromDate, @QueryParam("to") String toDate) { 
+    
+    log.info("run : {}", collectionCode); 
+    logic.run(institution, Util.getInstance().buildFilterMap(collectionCode, fromDate, toDate));
+     
     return Response.ok(success).build();
   }
 }
