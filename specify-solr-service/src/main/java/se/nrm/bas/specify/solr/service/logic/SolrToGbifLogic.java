f2a68c75fb5966bd63f75500595480bc9cfc1776
Ida Li <ida.li@nrm.se>
2020-02-28 16:12:14 +0100

    Add logic

---
+++ specify-solr-service/src/main/java/se/nrm/bas/specify/solr/service/logic/SolrToGbifLogic.java
@@ -0,0 +1,78 @@
+package se.nrm.bas.specify.solr.service.logic;
+ 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;  
+import javax.inject.Inject;
+import javax.json.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.utils.URIBuilder; 
+import se.nrm.bas.specify.gbif.datamodel.Logs;
+import se.nrm.bas.specify.gbif.datamodel.SimpleDwc;
+import se.nrm.bas.specify.solr.service.logic.jpa.GbifDao;
+import se.nrm.bas.specify.solr.service.logic.json.JsonConverter;
+import se.nrm.bas.specify.solr.service.logic.solr.SolrClient;
+import se.nrm.bas.specify.solr.service.logic.util.Util;
+
+/**
+ *
+ * @author idali
+ */
+@Slf4j
+public class SolrToGbifLogic {
+  
+  @Inject
+  private SolrClient solr;  
+  @Inject
+  private InitialProperties properties; 
+  @Inject 
+  private GbifDao dao;
+  
+  private final int maxFetchSize = 20; 
+  private final String nrm = "nrm"; 
+  private final String nrmCore = "nrm_index";
+  private final String gnmCore = "gnm_index";
+
+  
+  public void run(String institution, Map<String, String> filters) { 
+    log.info("run : {} - {}", institution, filters);
+    
+    boolean isNrm = institution.equals(nrm);
+    String searchQuery = Util.getInstance().buildSearchQuery(filters);
+    String core = isNrm ? nrmCore : gnmCore; 
+    URIBuilder builder = Util.getInstance().getUriBuilder(properties.getSolrPath(), core, searchQuery);
+     
+     
+    String result = solr.searchSolrData(builder, 0, maxFetchSize);  
+    JsonObject json = JsonConverter.getInstance().buildResponseJson(result);  
+    int numFound = JsonConverter.getInstance().getTotalNumberFound(json);
+    List<SimpleDwc> beans = JsonConverter.getInstance().mapEntities(json); 
+    saveEntities(beans, isNrm);
+   
+
+    if (numFound > maxFetchSize) {
+      for (int i = maxFetchSize + 1; i < numFound; i += maxFetchSize) {
+        result = solr.searchSolrData(builder, i, maxFetchSize);
+        json = JsonConverter.getInstance().buildResponseJson(result); 
+        beans = JsonConverter.getInstance().mapEntities(json); 
+        
+        saveEntities(beans, isNrm);
+      }
+    }
+    saveLogs(numFound, isNrm); 
+  }
+   
+  private void saveEntities(List<SimpleDwc> beans, boolean isNrm) {
+     beans.stream()
+            .forEach(bean -> {
+              dao.merge(bean, isNrm);
+            });
+  }
+  
+  private void saveLogs(int numFound, boolean isNrm) {
+    Logs logs = new Logs();
+    logs.setTimestamp(new Date());
+    logs.setTotalRecords(numFound);
+    dao.createLogs(logs, isNrm);
+  }
+}
